0af844a7186c22dbb66c7e52b6cbd7533d8bead7
Luqmaan Hakeem <rockgameplayhakeem@gmail.com>
2024-06-20 17:36:06 +0530

    code: created a test and added more functions for lexer

--- tokens/token.go
+++ tokens/token.go
@@ -6,13 +6,24 @@ type Token struct{
 	Type TokenType
 	Literal string
 }
+var keywords = map[string]TokenType{
+	"fn": FUNCTION,
+	"let": LET,
+}
 
+func LookUpIndent(ident string) TokenType{
+	if tok,ok:= keywords[ident];ok{
+		return tok
+	}
+	return IDENT
+}
 const (
 	ILLEGAL ="ILLEGAL"
 	EOF = "EOF"
 	IDENT ="IDENT"
 	INT ="INT"
 	ASSIGN ="="
+	SUBT = "-"
 	PLUS="+"
 	COMMA=","
 	SEMICOLON=";"
