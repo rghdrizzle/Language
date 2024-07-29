4ecfda800bc67e692e972404aac497d95dfbf507
Luqmaan Hakeem <rockgameplayhakeem@gmail.com>
2024-07-29 22:08:35 +0530

    code: added more functions to the parser to parse let statements

--- parser/parser.go
+++ parser/parser.go
@@ -44,7 +44,7 @@ func (p *Parser) ParseProgram() *ast.Program{
   return program
 }
 
-func (p *Parser) parseStatement() *ast.Statement{
+func (p *Parser) parseStatement() ast.Statement{
   switch p.curToken.Type{
   case token.LET:
     return p.parseLetStatement()
@@ -52,4 +52,35 @@ func (p *Parser) parseStatement() *ast.Statement{
     return nil
   }
 }
+func (p *Parser) parseLetStatement() *ast.LetStatement{
+  stmt:= &ast.LetStatement{Token: p.curToken}
+  if !p.expectPeek(token.IDENT) {
+    return nil
+    }
+    stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+    if !p.expectPeek(token.ASSIGN) {
+    return nil
+    }
+    //We're skipping the expressions until we encounter a semicolon
+    for !p.curTokenIs(token.SEMICOLON) {
+    p.nextToken()
+    }
+    return stmt
+}
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+  return p.curToken.Type == t
+  }
+  func (p *Parser) peekTokenIs(t token.TokenType) bool {
+  return p.peekToken.Type == t
+  }
+  func (p *Parser) expectPeek(t token.TokenType) bool {
+  if p.peekTokenIs(t) {
+  p.nextToken()
+  return true
+  } else {
+  return false
+  }
+  }
+
+
 
