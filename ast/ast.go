65daa954b2ab36b1aa7c7896d05512abe0733bd2
Luqmaan Hakeem <rockgameplayhakeem@gmail.com>
2024-09-24 20:20:28 +0530

    code: created ast for prefix exp and also tests for it

--- ast/ast.go
+++ ast/ast.go
@@ -62,7 +62,21 @@ func (il *IntegerLiteral) ExpressionNode() {}
 func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
 func (il *IntegerLiteral) String() string { return il.Token.Literal }
 
-
+type PrefixExpression struct{
+	Token token.Token
+	Operator string
+	Right Expression
+}
+func (pe *PrefixExpression) ExpressionNode() {}
+func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
+func (pe *PrefixExpression) String() string {
+	var out bytes.Buffer
+	out.WriteString("(")
+	out.WriteString(pe.Operator)
+	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+	return out.String()
+}
 func (p *Program) TokenLiteral() string{
 	if len(p.Statements)>0{
 		return p.Statements[0].TokenLiteral()
