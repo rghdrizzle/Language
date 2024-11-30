d1ad532b211157763b00d7de9333cc256a1b4743
Luqmaan Hakeem <rockgameplayhakeem@gmail.com>
2024-11-30 12:14:48 +0530

    code: created ast node and parser functions for function literals

--- ast/ast.go
+++ ast/ast.go
@@ -3,6 +3,7 @@ package ast
 import (
 	"bytes"
 	"rghdrizzle/language/tokens"
+	"strings"
 )
 
 type Node interface{
@@ -141,6 +142,30 @@ func (bs *BlockStatement) String() string {
 		out.WriteString(s.String())
 	}
 	return out.String()
+}
+
+
+type FunctionLiteral struct{
+	Token token.Token
+	Parameters []*Identifier
+	Body *BlockStatement
+}
+func (fl *FunctionLiteral) ExpressionNode() {}
+func (fl *FunctionLiteral) TokenLiteral() string { return fl.Token.Literal }
+func (fl *FunctionLiteral) String() string{
+	var out  bytes.Buffer
+
+	params := []string{}
+	for _ , p := range fl.Parameters{
+		params = append(params, p.String())
+	}
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(fl.Body.String())
+	return out.String()
+
 }
 
 
--- parser/parser.go
+++ parser/parser.go
@@ -83,6 +83,7 @@ func New(l *lexer.Lexer) *Parser{
   p.registerPrefix(token.LPAREN,p.parseGroupedExpression)
   p.registerPrefix(token.IF, p.parseIfExpression)
   p.registerPrefix(token.ELSE, p.parseIfExpression)
+  p.registerPrefix(token.FUNCTION, p.parseFunctionLiteral)
 
   return p
 }
@@ -307,4 +308,45 @@ func (p *Parser) parseBlockStatement() *ast.BlockStatement{
     p.nextToken()
   }
   return block
+}
+
+
+func (p *Parser) parseFunctionLiteral() ast.Expression{
+  lit := &ast.FunctionLiteral{Token: p.curToken}
+  if !p.expectPeek(token.LPAREN){
+    return nil
+  }
+  lit.Parameters = p.parseFunctionParameters()
+  if !p.expectPeek(token.LBRAC){
+    return nil
+  }
+  lit.Body = p.parseBlockStatement()
+
+  return lit
+
+}
+
+func (p *Parser) parseFunctionParameters() []*ast.Identifier{
+  identifiers := []*ast.Identifier{}
+
+  if p.peekTokenIs(token.RPAREN){
+    p.nextToken()
+    return identifiers
+  }
+  p.nextToken()
+  ident := &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+  identifiers = append(identifiers, ident)
+
+  for p.peekTokenIs(token.COMMA){
+    p.nextToken()
+    p.nextToken()
+    ident := &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+    identifiers = append(identifiers, ident)
+  }
+  if !p.expectPeek(token.RPAREN){
+    return nil
+  }
+
+  return identifiers
+
 }
