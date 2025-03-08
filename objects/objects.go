6daa83edb31fa359a61d264ebca8ad5478c662da
Luqmaan Hakeem <rockgameplayhakeem@gmail.com>
2025-03-08 19:07:31 +0530

    code: created builtin func object

--- objects/objects.go
+++ objects/objects.go
@@ -15,8 +15,10 @@ const (
 	ERROR_OBJ = "ERROR"
 	FUNCTION_OBJ = "FUNCTION"
 	STRING_OBJ = "STRING"
+	BUILTIN_OBJ = "BUILTIN"
 
 )
+type BuiltInFunction func(args ...Object) Object
 
 type ObjectType string
 
@@ -50,6 +52,10 @@ type String struct{
 }
 
 type Null struct{}
+
+type BuiltIn struct{
+	Fn BuiltInFunction
+}
 
 func (i *Integer) Inspect() string{
 	return fmt.Sprintf("%d",i.Value)
@@ -113,4 +119,12 @@ func (s *String) Type() ObjectType{
 
 func (s *String) Inspect() string{
 	return s.Value
+}
+
+func (b *BuiltIn) Type() ObjectType{
+	return BUILTIN_OBJ
+}
+
+func (b *BuiltIn) Inspect() string{
+	return "builtin function"
 }
