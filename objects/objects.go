cd51ee60eb5801405879dbecbaa797aee58311a0
Luqmaan Hakeem <rockgameplayhakeem@gmail.com>
2025-06-20 19:46:18 +0530

    code: created hashkey method to solve issue with keys in hashmaps

--- objects/objects.go
+++ objects/objects.go
@@ -5,6 +5,7 @@ import (
 	"rghdrizzle/language/ast"
 	"strings"
 	"bytes"
+	"hash/fnv"
 )
 
 const (
@@ -59,6 +60,10 @@ type Null struct{}
 
 type BuiltIn struct{
 	Fn BuiltInFunction
+}
+type HashKey struct{
+	Type ObjectType
+	Value uint64
 }
 
 func (i *Integer) Inspect() string{
@@ -148,4 +153,24 @@ func (ao *Array) Inspect() string{
 	out.WriteString("]")
 
 	return out.String()
+}
+
+func (b *Boolean) HashKey() HashKey{
+	var value uint64
+	 if b.Value {
+		value =1
+	 }else{
+		value =0
+	 }
+	 return HashKey{Type: b.Type(),Value: value}
+}
+
+func (i *Integer) HashKey() HashKey {
+	return HashKey{Type: i.Type(), Value: uint64(i.Value)}
+}
+func (s *String) HashKey() HashKey{
+	h := fnv.New64a()
+	h.Write([]byte(s.Value))
+
+	return HashKey{Type: s.Type(),Value: h.Sum64()}
 }
